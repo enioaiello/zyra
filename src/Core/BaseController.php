9057ab3ab34a11f414b86e429f57b0ae5245baba
Enio Aiello <eaiello@edenschool.fr>
2025-04-03 15:01:40 +0200

    Arborescence terminée

--- src/Core/BaseController.php
+++ src/Core/BaseController.php
@@ -10,6 +10,16 @@ class BaseController {
         }
 
         extract($params); // Transforme le tableau en variables PHP
+
+        var_dump(__DIR__);
+
+        // Inclure le fichier de header
+        require __DIR__ . '../../../includes/header.inc.php';
+
+        // Inclure le fichier de vue
         require $viewPath;
+
+        // Inclure le fichier de footer
+        require __DIR__ . '../../../includes/footer.inc.php';
     }
 }
